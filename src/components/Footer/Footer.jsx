d79f6e5616668230eabe27135ed4b39c5487e672
red-56 <adimired@gmail.com>
2020-03-23 10:02:08 +0100

    cl

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+
+/*eslint-disable*/
+import React from "react";
+// used for making the prop types of this component
+import PropTypes from "prop-types";
+
+// reactstrap components
+import { Container, Row, Nav, NavItem, NavLink } from "reactstrap";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="footer">
+      </footer>
+    );
+  }
+}
+
+export default Footer;
